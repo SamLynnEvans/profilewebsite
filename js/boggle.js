d57a632f65bab9229538616818431e549fa735a3
Sam Lynn-Evans <samuellynnevans@gmail.com>
2017-06-12 18:29:20 +0100

    first attempt using stringGen code

---
+++ js/boggle.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	function stringGen1(len)
+{
+    var text = " ";
+
+    var charset = "abcdefghijklmnopqrstuvwxyz";
+
+    for( var i=0; i < len; i++ )
+        text += charset.charAt(Math.floor(Math.random() * charset.length));
+
+    return text;
+}
+
+	function stringGen2(len)
+{
+    var text = " ";
+
+    var charset = "abcdefghijklmnopqrstuvwxyz";
+
+    for( var i=0; i < len; i++ )
+        text += charset.charAt(Math.floor(Math.random() * charset.length));
+
+    return text;
+}
+
+  var stringGen1v = $stringGen1(1).val();
+
+  alert (stringGen1v);
+
+$('#start').click(function(){
+	$('#1').append('<p>' + stringGen1(1) + '</p>')
+	$('#2').append('<p>' + stringGen2(1) + '</p>')
+})
+
+});
