10ddc6b7ee2916ea56fba13f1c3c83a231f24097
Sam Lynn-Evans <samuellynnevans@gmail.com>
2017-06-19 14:34:57 +0100

    working alternate clicks

--- js/match.js
+++ js/match.js
@@ -4,21 +4,35 @@ $(document).ready(function(){
 
 	
 	$(document.body).on('click', '.invisible', function(e) {
-  var clicks = $(this).data('clicks');
-  if (clicks) {
+	
+	
+
+    if(clicks % 2 == 0){
+    	$(this).removeClass('invisible');
+        alert('1')
+    }else{
+    	$(this).removeClass('invisible');
+        alert('2')
+    }
+    ++clicks;
+
+
+
+});
+
+});
+
+
+
+	  /*var clicks = $('.invisible').data('clicks');
+  if (clicks == 1) {
+  	$(this).removeClass('invisible');
      alert('1')
   } else {
      alert('2')
+     $(this).removeClass('invisible');
   }
-  $(this).data("clicks", !clicks);
-
-
-
-});
-
-});
-
-
+  $(this).data("clicks", !clicks);*/
     /*if(clicks == 0){
          	$(this).removeClass('invisible');  
    	alert('1');
